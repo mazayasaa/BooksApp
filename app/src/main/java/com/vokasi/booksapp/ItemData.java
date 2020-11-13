8172b906a8f5752bfe3f31f293bb7ce3ad4d8856
mazayasaa <aya.nurkholik@gmail.com>
2020-11-13 10:52:52 +0700

    Initial commit

---
+++ app/src/main/java/com/vokasi/booksapp/ItemData.java
@@ -0,0 +1,8 @@
+package com.vokasi.booksapp;
+
+public class ItemData {
+    public String itemTitle;
+    public String itemAuthor;
+    public String itemDescription;
+    public String itemImage;
+}
